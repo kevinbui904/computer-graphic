4fabdc28b6d1d1e558ef2c59b62740afce499088
Thien K. M. Bui <kevinbui904@gmail.com>
2022-09-13 18:06:32 -0500

    copied in files

---
+++ triangle-rasterization/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author Thien K. M. Bui <buik@carleton.edu>
+ * @brief triangle rasterization project
+ * @version 0.1
+ * @date 2022-09-13
+ *
+ * @copyright Copyright (c) 2022 Thien K. M. Bui
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world\n");
+}
